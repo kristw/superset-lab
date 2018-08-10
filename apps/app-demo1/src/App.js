0b299c8dd83a5779b067b44060bc67d9bad7ca2d
Krist Wongsuphasawat <krist.wongz@gmail.com>
2018-08-09 22:04:18 -0700

    scaffold demo app and delete old app

--- apps/app-demo1/src/App.js
+++ apps/app-demo1/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { configure, SuperChart } from './@kristw-lab-superset/core';
+import { configure, SuperChart } from '@kristw-lab-superset/core';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <h1>Test Plugin</h1>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
           <SuperChart type="abc" />
           <SuperChart type="json" />
         </p>
